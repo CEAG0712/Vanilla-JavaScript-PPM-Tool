a051af0786721916ac1faf26a9ca92218b1fa826
Carlos Arosemena <carlosarosemena@Carloss-MacBook-Pro-2.local>
2024-09-29 15:09:33 -0400

    weback setup

---
+++ FRONT-END/src/h1-title.js
@@ -0,0 +1,5 @@
+const h1Title = {
+  title: "generate title from h1-title.js",
+};
+
+export default h1Title;
---
+++ FRONT-END/src/index.js
@@ -0,0 +1,6 @@
+import h1Title from "./h1-title.js";
+const testH1 = document.getElementById("test-h1");
+
+testH1.innerHTML = h1Title.title;
+
+console.log("webpack setup");
