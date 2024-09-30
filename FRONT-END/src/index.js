f0296615f06349a6ce6ee46c78b549956c645e0b
Carlos Arosemena <carlosarosemena@Carloss-MacBook-Pro-2.local>
2024-09-30 10:50:13 -0400

    part II

--- FRONT-END/src/index.js
+++ FRONT-END/src/index.js
@@ -1,1 +1,16 @@
 import "./styles/stylekanban.css";
+import routes from "./routes.js";
+
+export const router = async () => {
+  const app = document.getElementById("app");
+  const path = location.pathname || "/";
+  const route = routes[path]; // " / "
+  console.log("app ", app);
+  console.log("path ", path);
+  console.log("route ", route);
+
+  // try {
+  // } catch (error) {}
+};
+
+router()
