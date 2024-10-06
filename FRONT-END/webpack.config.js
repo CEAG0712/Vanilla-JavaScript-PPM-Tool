ba18bae846b67bd1df5250110da33c73ab2b776a
Carlos Arosemena <carlosarosemena@Carloss-MacBook-Pro-2.local>
2024-10-06 11:30:27 -0400

    sd

--- FRONT-END/webpack.config.js
+++ FRONT-END/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "src"),
+      directory: path.resolve(__dirname, "dist"),
     },
     port: 3001,
     open: true,
